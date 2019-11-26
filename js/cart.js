bf79b06ce0abbd3ccd9d60cef0149c653c90182e
Joanna Yapor <joannayapor@gmail.com>
2019-11-26 15:50:29 -0300

    Agregar pino al carrito

--- js/cart.js
+++ js/cart.js
@@ -8,30 +8,63 @@ const CREDIT_CARD_PAYMENT = "Tarjeta de crédito";
 const BANKING_PAYMENT = "Transferencia bancaria";
 let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 
+
 //Función que se utiliza para actualizar los costos de publicación
-function updateTotalCosts(){
+function updateTotalCosts() {
+}
+
+function updateSubtotal() {
+    productCount = document.getElementById("productCountInput").value;
+    subtotal = productCount * productCost;
+    document.getElementById("productCostText").innerHTML = subtotal;
+    document.getElementById("SubTotal").innerHTML = subtotal;
+
 
 }
 
-function updateSubtotal(){
+function showPaymentTypeNotSelected() {
 
 }
 
-function showPaymentTypeNotSelected(){
+function hidePaymentTypeNotSelected() {
 
 }
+//  Esto me muestra el pinito y todas sus atributos en el carrito
+function showProductList(currentProductArray) {
+    let htmlContentToAppend = "";
+    for (let i = 0; i < currentProductArray.length; i++) {
+        let product = currentProductArray[i];
+        htmlContentToAppend += `
+            <a href="cart.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.src + `" alt="` + product.name + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name + `</h4>
+                            <small class="text-muted">` + product.currency + " " + product.unitCost + ` artículos</small>
+                        </div>                    
+                        <input type="number" id="productCountInput" onClick= "updateSubtotal()"  value= "` + product.count + `" min="1"> </input>
+                        <p class="mb-1">` + product.currency + " " + ` <a id=SubTotal> </a> </p> 
+                    </div>
+                </div>
+             `
 
-function hidePaymentTypeNotSelected(){
-
-}
-
-function showProducts(products){
-
-}
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+    updateSubtotal();
+};
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(CART_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            console.log(resultObj);
+            //showProductList(resultObj.data.articles);
+            updateSubtotal();
+        }
+    });
 });
--- js/product-info.js
+++ js/product-info.js
@@ -2,6 +2,21 @@ var product = {};
 
 function showImagesGallery(array){
 
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){
+        let images = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + images + `" alt="">
+            </div>
+        </div>
+        `
+
+        document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
+    }
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -18,7 +33,19 @@ document.addEventListener("DOMContentLoaded", function(e){
 
             let productDescriptionHTML = document.getElementById("productDescription");
             productDescriptionHTML.innerHTML = product.description;
-// continuar
+
+            let productCountHTML = document.getElementById("productSoldCount");
+            productCountHTML.innerHTML = product.soldCount;
+
+            let productCostHTML = document.getElementById("productCost");
+            productCostHTML.innerHTML = product.cost;
+
+            let productCurrencyHTML = document.getElementById("productCurrency");
+            productCurrencyHTML.innerHTML = product.currency;
+
+            showImagesGallery(product.images);
+//Llama los productos del Json y muestra las caracteristicas del producto.
+// Con la función showImagesGallery lo que hago es mostrar la imagenes n forma de galeria.
         }
     });
 });
